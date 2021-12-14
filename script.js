ddf3f9d2ca94f6d33159d03dc14ec84a577e72ac
Ashik S Nair <aashiq5342@gmail.com>
2021-12-14 16:04:02 +0530

    nav toggle active modified

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var nav = document.querySelector("nav");
 var nav_a = document.querySelectorAll(".nav-a");
 var nav_section = document.querySelector(".nav-section");
 
+// nav section toggling starts
+
 nav_toggle_section.addEventListener("click", () => {
   nav_toggle1.classList.toggle("active");
   nav_toggle2.classList.toggle("active");
@@ -22,10 +24,16 @@ document.addEventListener("click", (e) => {
   }
 });
 
-window.addEventListener('scroll', () => {
-  if(pageYOffset > (window.innerHeight - 100)){
-    nav_section.classList.add('sticky');
-  }else{
-    nav_section.classList.remove('sticky');
+// nav section toggling ends
+
+// nav styling on scroll starts
+
+window.addEventListener("scroll", () => {
+  if (pageYOffset > window.innerHeight - 100) {
+    nav_section.classList.add("sticky");
+  } else {
+    nav_section.classList.remove("sticky");
   }
-})
+});
+
+// nav styling on scroll ends
